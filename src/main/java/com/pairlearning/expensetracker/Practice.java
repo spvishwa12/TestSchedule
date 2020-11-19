14fdeb1c817fea6e4667ed7eca91ea5ecd3cbca7
spvishwa12 <sp.vishwa12@gmail.com>
2020-11-19 11:38:21 +0530

    added new file

---
+++ src/main/java/com/pairlearning/expensetracker/Practice.java
@@ -0,0 +1,14 @@
+package com.pairlearning.expensetracker;
+
+public class Practice {
+
+	public Practice() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
